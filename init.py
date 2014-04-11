2d4924685440add5393bd00b15fe16da6aa1b24c
Nate Mara <natemara@gmail.com>
2014-04-11 19:56:30 -0400

    got init script working

--- init.py
+++ init.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python3
 import os
+import sys
+
+files = [
+		'aliases',
+		'bashrc',
+		'tmux.conf'
+]
 
 
 def main():
-	dotfiles_dir = os.path.dirname(os.path.realpath(__file__))
+	parse_for_task()
 
-	files = [
-				'aliases',
-				'bashrc',
-				'tmux.conf'
-			]
+
+def parse_for_task():
+	arglist = sys.argv[1:]
+	if 'link' in arglist:
+		make_all_links()
+		return
+	if 'clean' in arglist:
+		clean_all_links()
+		return
+	make_all_links()
+
+
+def clean_all_links():
+	for file in files:
+		if exists(file):
+			os.remove(dotfile_path(file))
+
+
+def make_all_links():
+	dotfiles_dir = os.path.dirname(os.path.realpath(__file__))
 
 	for file in files:
 		if not exists(file):
@@ -23,16 +45,16 @@ def main():
 
 def safe_to_link(file):
 	path = dotfile_path(file)
-	is_link = os.path.islink(dotfile_path)
+	is_link = os.path.islink(path)
 	if not is_link:
 		delete = input('{0} exists, and is not a link, delete? (y/n)'.format(file))
 		if delete.lower() == 'y':
-			os.remove(dotfile_path)
+			os.remove(path)
 			return True
 		return False
 	delete = input('{0} exists, and is a link, delete? (y/n)'.format(file))
 	if delete.lower() == 'y':
-		os.remove(dotfile_path)
+		os.remove(path)
 		return True
 	return False
 
@@ -41,22 +63,22 @@ def dotfile_path(file):
 	dotfile = '.' + file
 	home_path = os.path.expanduser('~')
 	path = os.path.join(home_path, dotfile)
+	path = os.path.abspath(path)
 	return path
 
 
 def exists(file):
 	file_path = dotfile_path(file)
-	file_path = os.path.abspath(file_path)
-	print("'{0}'".format(file_path))
-	print(type(file_path))
 	dotfile_exists = os.path.exists(file_path)
-	print(dotfile_exists)
 	return dotfile_exists
 
 
 def make_link(file):
-	path = dotfile_path(file)
-	
+	link_path = dotfile_path(file)
+	file_path = os.path.abspath(file)
+	os.symlink(file_path, link_path)
+	print('Linking ./{0} to {1}...'.format(file, link_path))
+	print()
 
 
 if __name__ == '__main__':
